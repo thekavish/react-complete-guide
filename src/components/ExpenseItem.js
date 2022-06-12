9fff6bb37c1935ab044473efcfd6fe7a08c7ccc8
Kavish Kavish <kavish.kavish@alight.com>
2022-06-13 01:34:39 +0530

    added expense date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-    const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-
     return (
         <div className="expense-item">
-            <div>{props.date.toLocaleDateString("en-US", options)}</div>
+            <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">Rs. {props.amount}</div>
