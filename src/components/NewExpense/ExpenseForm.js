9e422795feed47fa674e28dbb671d7fad55cfb98
Kavish Patel <kavish619@gmail.com>
2022-06-23 12:20:42 +0530

    added new expense form validation

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,20 +44,20 @@ const ExpenseForm = props => {
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label htmlFor="title">Title</label>
-                <input type="text" id="title"
+                <input type="text" id="title" required
                        value={userInputs.inputTitle}
                        onChange={titleChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label htmlFor="amount">Amount</label>
                 <input type="number" id="amount" min="0.01" step="0.01"
-                       value={userInputs.inputAmount}
+                       required value={userInputs.inputAmount}
                        onChange={amountChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label htmlFor="date">Date</label>
                 <input type="date" id="date" min="2019-01-01" max="2024-12-31"
-                       value={userInputs.inputDate}
+                       required value={userInputs.inputDate}
                        onChange={dateChangeHandler}/>
             </div>
         </div>
